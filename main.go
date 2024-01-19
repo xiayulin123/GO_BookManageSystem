c7f19da7283cd49bb14e65017e6161db57f93966
xiayulin123 <xiayulinsky@gmail.com>
2024-01-18 22:20:07 -0500

    routers

--- main.go
+++ main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/xiayulin123/GO_Ecommerce/controllers"
+	"github.com/xiayulin123/GO_Ecommerce/database"
+	"github.com/xiayulin123/GO_Ecommerce/middleware"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	app := controllers.NewApplication(database.ProductData(database.Client, "Products"), database.UserData(database.Client, "Users"))
+
+	router := gin.new()
+
+	router.Use(gin.Logger())
+
+	routers.UserRoutes(router)
+
+	router.Use(middleware.Authentication())
+}
