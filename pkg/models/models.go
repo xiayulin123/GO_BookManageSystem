45f5d68bc13f5a972db33ba832738888286ba67c
xiayulin123 <xiayulinsky@gmail.com>
2024-01-22 20:22:36 -0500

    add models

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -1,1 +1,21 @@
 package models
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/xiayulin123/Go-bookstore/pkg/config"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `gorm:"json:"name"`
+	Author      string `gorm:"json:"author"`
+	Publication string `gorm:"json:"publication"`
+}
+
+func init() {
+	config.Connection()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
