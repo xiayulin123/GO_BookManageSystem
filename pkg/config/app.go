43f6eadaaa8a04b0182e2be6a80b6c5009dac4b4
xiayulin123 <xiayulinsky@gmail.com>
2024-01-22 20:20:48 -0500

    add utils

---
+++ pkg/config/app.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connection() {
+	d, err := gorm.Open("mysql", "yulin:12345678/simplerest?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
